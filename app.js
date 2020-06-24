91c0322ee586128c2b8895fc4938d1247a26bce9
Arrey Tabe <arrey2391@gmail.com>
2020-06-24 23:26:03 +0100

    added project files

---
+++ app.js
@@ -0,0 +1,20 @@
+var budgetController = (function () {
+    var x = 23;
+    var add = function (a) {
+        return x + a;
+    }
+
+    return {
+        publicTest: function (b) {
+            console.log(add(b));
+        }
+    }
+
+})();
+
+
+var UIController = (function () {
+
+    //some code
+    var
+})();
