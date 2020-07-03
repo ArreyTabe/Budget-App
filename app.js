107fc8a3f9aa1d13d793b2a21472e391daa841e5
Arrey Tabe <arrey2391@gmail.com>
2020-07-03 16:04:43 +0100

    displaying reuslts to the UI

--- app.js
+++ app.js
@@ -100,7 +100,11 @@ var UIController = (function () {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     }
     return {
         getInput: function () {
@@ -149,6 +153,19 @@ var UIController = (function () {
             });
             fieldsArr[0].focus();
         },
+        displayBudget: function (obj) {
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+
+            if (obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+
+            }
+
+        },
 
         getDOMstrings: function () {
             return DOMstrings;
@@ -186,7 +203,7 @@ var controller = (function (budgetctrl, UIcntrl) {
         var budget = budgetctrl.getBudget();
 
         //3 didplay the result on the UI
-        console.log(budget);
+        UIcntrl.displayBudget(budget);
 
 
     };
@@ -220,6 +237,12 @@ var controller = (function (budgetctrl, UIcntrl) {
     return {
         init: function () {
             console.log('Application started!');
+            UIcntrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
             setupEventListeners();
         }
     };
