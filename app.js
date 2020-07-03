f095ef8f51db7aad6c860e494ece50da8e5b22bd
Arrey Tabe <arrey2391@gmail.com>
2020-07-03 13:18:18 +0100

    convert list to an array, use forEach method  to clear input field and add focus

--- app.js
+++ app.js
@@ -102,6 +102,19 @@ var UIController = (function () {
 
         },
 
+        clearFields: function () {
+            var fields, fieldsArr;
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function (current, index, array) {
+                current.value = '';
+
+            });
+            fieldsArr[0].focus();
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -142,8 +155,12 @@ var controller = (function (budgetctrl, UIcntrl) {
 
         //3 add the item to the UI
         UIcntrl.addListItem(newItem, input.type);
-        //4 calculate the budget
-        // didplay the result on the UI
+
+        //4 clear the fields
+        UIcntrl.clearFields();
+        //5 calculate the budget
+
+        //6 didplay the result on the UI
 
     }
 
